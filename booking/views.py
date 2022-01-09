15898a89b8c75a072d74f62d8ffe4ea6c3754992
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-09 22:35:43 +0000

    add BookingList class to views.py

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+class BookingList(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.order_by('-date')
+    template_name = 'booking.html'
