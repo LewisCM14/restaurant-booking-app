37b6fc2cbaaa6137e42a51dd727748ac8220e439
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-15 22:18:24 +0000

    render BookingForm in browser

--- booking/forms.py
+++ booking/forms.py
@@ -4,13 +4,13 @@ from django import forms
 
 class BookingForm(forms.ModelForm):
     """
-    Create our CommentForm class that inherits from the base form.
+    Create our BookingForm class that inherits from the base form.
     """
     class Meta:
         """
-        Telling our comment form what model to use,
+        Telling our booking form what model to use,
         then which fields we want displayed on our form.
         trailing comma means python reads as tuple, not string
         """
         model = Booking
-        fields = ('body',)
+        fields = ('lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests',)
