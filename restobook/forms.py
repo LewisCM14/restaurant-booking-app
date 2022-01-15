07eb5ce8ecbd886d44de257da4e82f893647ba96
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-15 19:34:19 +0000

    adjust nav bar links, revert signup form

--- restobook/forms.py
+++ restobook/forms.py
@@ -8,10 +8,12 @@ class CustomSignupForm(SignupForm):
     adds custom sign up fields.
     """
 
-    full_name = forms.CharField(max_length=50, label='Full Name')
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
-        user.full_name = self.cleaned_data['full_name']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.save()
         return user
