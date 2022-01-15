0c1b3eda32bc4a27b236545c60dafa6b194b0327
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-15 20:48:08 +0000

    wire up booking.html

--- booking/views.py
+++ booking/views.py
@@ -3,12 +3,16 @@ from django.views import generic
 from .models import Booking
 
 
-class BookingList(generic.ListView):
-    model = Booking
-    queryset = Booking.objects.order_by('-date')
-    template_name = 'booking.html'
-    paginate_by = 6
+# class BookingList(generic.ListView):
+#     model = Booking
+#     queryset = Booking.objects.order_by('-date')
+#     template_name = 'booking.html'
+#     paginate_by = 6
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def booking(request):
+    return render(request, 'booking.html', {})
