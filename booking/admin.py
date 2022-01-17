df1906ac77037ea5dd559c1c45ce4086dea39cb7
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-17 22:08:44 +0000

    adjust BookingAdmin display

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('lead', 'date', 'time', 'guests', 'status',)
