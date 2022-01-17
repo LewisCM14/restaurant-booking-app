f8b6fb90136c7c88e8df371b8486a7a805b10850
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-17 22:19:29 +0000

    add docstring to BookingAdmin

--- booking/admin.py
+++ booking/admin.py
@@ -4,6 +4,12 @@ from .models import Booking
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
+    """
+    Allows admins a quick overview of all bookings,
+    with the ability to filter by date and time for a precise overview
+    intended for use when seating walk-in customers on a given day.
+    Also allows for search by booking lead.
+    """
     list_display = ('lead', 'date', 'time', 'guests', 'status',)
     list_filter = ('date', 'time',)
     search_fields = ('lead',)
