49639e2a9ece4536ad7f02c15c04298c4febfead
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-17 23:01:06 +0000

    add accept and decline methods to BookingAdmin

--- booking/admin.py
+++ booking/admin.py
@@ -9,7 +9,22 @@ class BookingAdmin(admin.ModelAdmin):
     with the ability to filter by date and time for a precise overview
     intended for use when seating walk-in customers on a given day.
     Also allows for search by booking lead.
+
+    Containts methods to accept or decline the bookings within the dropdown.
     """
     list_display = ('lead', 'date', 'time', 'guests', 'status',)
     list_filter = ('date', 'time',)
     search_fields = ('lead',)
+    actions = ['accept_booking', 'decline_booking']
+
+    def accept_booking(self, request, queryset):
+        """
+        Allows bookings to be accepted from the dropdown menu in admin.
+        """
+        queryset.update(status=1)
+
+    def decline_booking(self, request, queryset):
+        """
+        Allows bookings to be declined from the dropdown menu in admin.
+        """
+        queryset.update(status=2)
