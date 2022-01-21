496aef0c1b00bdaacd70769ad18eb6d4819f9399
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-21 21:23:05 +0000

    update BookingForm docstrings

--- booking/forms.py
+++ booking/forms.py
@@ -4,13 +4,14 @@ from django import forms
 
 class BookingForm(forms.ModelForm):
     """
-    Create our BookingForm class that inherits from the base form.
+    Creates the BookingForm class that inherits from the base form.
     """
     class Meta:
         """
-        Telling our booking form what model to use,
-        then which fields we want displayed on our form.
-        trailing comma means python reads as tuple, not string
+        The booking form inherits from the Booking model.
+        Takes the required fields needed for a user to make a new booking.
         """
         model = Booking
-        fields = ('lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests',)
+        fields = (
+            'lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests',
+        )
