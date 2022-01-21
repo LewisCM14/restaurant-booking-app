324b5891a67469ecd3f4792112055a57fb21a84c
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-21 21:17:05 +0000

    update Booking model docstrings

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from autoslug import AutoSlugField
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 from cloudinary.models import CloudinaryField
 
 
@@ -14,30 +14,36 @@ STATUS = ((0, "pending"), (1, "accepted"), (2, "declined"))
 
 class Booking(models.Model):
     """
-    The model for the booking app, stores the booking: lead,
-    date, time and how many guests for the individual booking.
-    Defaults the booking status to 'pending' using the above tuple.
-    The notes allows for special requirements to be detailed.
-    The slug field, generated from the email plus date & time of the booking,
-    will be used to identify each unique/individual booking.
+    The model for the booking app.
 
-    The meta class orders each booking by reservation date in descending order.
-    str returns the reservation date and time to be used as booking title.
+    Inherits the user from account sign up.
+    Stores the booking: lead, email, mobile, date, time
+    and how many guests for each individual booking.
+    Plus any special requirement notes.
+
+    Defaults the booking status to 'pending' using the above tuple.
+
+    Slug field is generated using autoslug.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    lead = models.CharField(max_length=200, blank=False)  # needs to be taken from user account
-    email = models.CharField(max_length=200, blank=False)  # needs to be taken from user account
+
+    lead = models.CharField(max_length=200, blank=False)
+    email = models.CharField(max_length=200, blank=False)
     mobile = models.IntegerField(blank=False)
     date = models.DateField(blank=False)
     time = models.TimeField(blank=False)
     notes = models.TextField(max_length=200)
     guests = models.PositiveIntegerField(blank=False)
+
     status = models.IntegerField(choices=STATUS, default=0)
     slug = AutoSlugField(populate_from='email')
 
     class Meta:
-        """ Orders individual bookings by date """
+        """ Orders individual bookings by date in descending order. """
         ordering = ['-date']
 
     def __str__(self):
+        """
+        Returns the reservation date and time to be used as booking title.
+        """
         return f'{self.date} {self.time}'
