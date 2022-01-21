60a2bf4dc557ccda36848693f461db4672041768
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-21 21:19:32 +0000

    update BookingAdmin docstrings

--- booking/admin.py
+++ booking/admin.py
@@ -6,9 +6,9 @@ from .models import Booking
 class BookingAdmin(admin.ModelAdmin):
     """
     Allows admins a quick overview of all bookings,
-    with the ability to filter by date and time for a precise overview
-    intended for use when seating walk-in customers on a given day.
-    Also allows for search by booking lead.
+    with the ability to filter by date and time for a precise overview.
+    intended for use when seating walk-in customers on a given day or
+    making business decsions. Also allows for search by booking lead.
 
     Containts methods to accept or decline the bookings within the dropdown.
     """
