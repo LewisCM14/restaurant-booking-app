d3886ae6d760f30affd702791f5854a215801b2b
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-24 19:29:03 +0000

    add docstrings to test_forms

--- booking/test_forms.py
+++ booking/test_forms.py
@@ -5,11 +5,27 @@ from . forms import BookingForm
 class TestBookingForm(TestCase):
 
     def test_lead_is_required(self):
+        """
+        Creates an instance of the BookingForm with the lead field empty.
+        Then uses assert false to confirm this.
+
+        Also sends back a dictionary of fields on which there were errors
+        and the Associated error messages. Then uses assert in,
+        to confirm theirs a lead key in the dictionairy of form errors.
+
+        Then uses assert equal to check whether the error message
+        on the lead field is 'This field is required.'
+        """
         form = BookingForm({'lead': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('lead', form.errors.keys())
         self.assertEqual(form.errors['lead'][0], 'This field is required.')
 
     def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Creates an empty instance of the BookingForm,
+        then use assert equal to check whether the form.meta.fields attribute
+        is equal to the fields specified in forms.py.
+        """
         form = BookingForm()
         self.assertEqual(form.Meta.fields, ('lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests'))
