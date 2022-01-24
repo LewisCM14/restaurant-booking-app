63efdeadf518d2b7e81338d9e6143425f942674d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-24 20:10:35 +0000

    create test_notes_not_required

--- booking/test_forms.py
+++ booking/test_forms.py
@@ -3,11 +3,16 @@ from . forms import BookingForm
 
 
 class TestBookingForm(TestCase):
+    """
+    Contains the tests for the BookingForm.
+    Located in the booking app in forms.py.
+    """
 
     def test_lead_is_required(self):
         """
         Creates an instance of the BookingForm with the lead field empty.
-        Then uses assert false to confirm this.
+        Then uses assert false on form.is_valid,
+        this returns false, confirming the lead field is required.
 
         Also sends back a dictionary of fields on which there were errors
         and the Associated error messages. Then uses assert in,
@@ -56,7 +61,7 @@ class TestBookingForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('time', form.errors.keys())
         self.assertEqual(form.errors['time'][0], 'This field is required.')
-    
+
     def test_guests_is_required(self):
         """
         Follows same logic for test_lead_is_required
@@ -66,6 +71,25 @@ class TestBookingForm(TestCase):
         self.assertIn('guests', form.errors.keys())
         self.assertEqual(form.errors['guests'][0], 'This field is required.')
 
+    def test_notes_is_not_required(self):
+        """
+        Creates an instance of the BookingForm,
+        with only the 'notes' field blank.
+        Then uses assert true to make sure the form is valid.
+        This confirms that even with notes left blank, the form is still valid,
+        meaning notes isn't required on the BookingForm.
+        """
+        form = BookingForm({
+            'lead': 'Booking Lead',
+            'email': 'test@email.com',
+            'mobile': '01509',
+            'date': 'JAN. 24, 2022',
+            'time': '8 P.M.',
+            'notes': '',
+            'guests': '2'
+        })
+        self.assertTrue(form.is_valid())
+
     def test_fields_are_explicit_in_form_metaclass(self):
         """
         Creates an empty instance of the BookingForm,
@@ -73,4 +97,6 @@ class TestBookingForm(TestCase):
         is equal to the fields specified in forms.py.
         """
         form = BookingForm()
-        self.assertEqual(form.Meta.fields, ('lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests'))
+        self.assertEqual(form.Meta.fields, (
+            'lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests'
+            ))
