599965626e67f62468c3330a748db15bd3c0ee03
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-25 16:36:58 +0000

    pass test_get_amend_booking

--- booking/test_views.py
+++ booking/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-  
+from .models import Booking
+
 
 class TestViews(TestCase):
     """
@@ -18,7 +19,7 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html', 'base.html')
-    
+
     def test_get_booking_page(self):
         """
         Uses Django's in-built HTTP client to get /makebooking URL.
@@ -30,18 +31,39 @@ class TestViews(TestCase):
         response = self.client.get('/makebooking')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'booking.html', 'base.html')
-    
+
     def test_get_amend_booking_page(self):
-        pass
-    
+        """
+        Uses the Booking Model import to create a reservation instance.
+        Then uses Django's in-built HTTP client,
+        to get /amend/reservation.id URL. Asserts equal to status code 200,
+        a successful HTTP response.
+
+        Then uses assert Template Used to ensure the booking.html page,
+        plus the base.html it is extended from, is being used.
+        """
+        reservation = Booking.objects.create(
+            lead='Test Lead',
+            email='test@email.com',
+            mobile='01509',
+            date='JAN. 25, 2022',
+            time='4:30 P.M.',
+            notes='test',
+            guests='2'
+            )
+
+        response = self.client.get(f'/amend/{reservation.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'amend_booking.html', 'base.html')
+
     def test_get_reservations_page(self):
         pass
-    
+
     def test_create_booking_request(self):
         pass
-    
+
     def test_cancel_booking_request(self):
         pass
-    
+
     def test_amend_booking_request(self):
         pass
