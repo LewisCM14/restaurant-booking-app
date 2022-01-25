c1dcad7a6195bfd37b312b54d44a2d1c910a5f21
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-25 16:10:38 +0000

    begin testing views

--- booking/test_views.py
+++ booking/test_views.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def test_get_index_page(self):
+        pass
+    
+    def test_get_booking_page(self):
+        pass
+    
+    def test_get_amend_booking_page(self):
+        pass
+    
+    def test_get_reservations_page(self):
+        pass
+    
+    def test_create_booking_request(self):
+        pass
+    
+    def test_cancel_booking_request(self):
+        pass
+    
+    def test_amend_booking_request(self):
+        pass
