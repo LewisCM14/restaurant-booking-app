a7b4c24883828b8f1c646e7b14230170833c6edd
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-25 16:22:53 +0000

    pass test_get_booking_page

--- booking/test_views.py
+++ booking/test_views.py
@@ -20,7 +20,16 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'index.html', 'base.html')
     
     def test_get_booking_page(self):
-        pass
+        """
+        Uses Django's in-built HTTP client to get /makebooking URL.
+        Asserts equal to status code 200, a successful HTTP response.
+
+        Then uses assert Template Used to ensure the booking.html page,
+        plus the base.html it is extended from, is being used.
+        """
+        response = self.client.get('/makebooking')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booking.html', 'base.html')
     
     def test_get_amend_booking_page(self):
         pass
