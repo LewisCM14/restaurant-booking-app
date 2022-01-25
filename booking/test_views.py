a7e1cb82d6a05f3e33f1e12fce48f3c2d826ccd1
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-25 16:20:17 +0000

    pass test_get_index_page

--- booking/test_views.py
+++ booking/test_views.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
+  
 
 class TestViews(TestCase):
+    """
+    Contains the tests for the views.
+    Located in the booking app in vies.py.
+    """
 
     def test_get_index_page(self):
-        pass
+        """
+        Uses Django's in-built HTTP client to get the index page URL slash.
+        Asserts equal to status code 200, a successful HTTP response.
+
+        Then uses assert Template Used to ensure the index.html page,
+        plus the base.html it is extended from, is being used.
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html', 'base.html')
     
     def test_get_booking_page(self):
         pass
