ed2a9fc06034bf68315c555643c15a4f116048b3
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-27 17:36:47 +0000

    create test_admin.py

---
+++ booking/test_admin.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Booking
+
+
+# class TestAdmin(TestCase):
+#     """
+#     Contains the tests for the admin methods.
+#     Located in the booking app in admin.py.
+#     """
+
+#     def test_admin_can_manage_booking_status(self):
+#         """"""
+#         test_user = User.objects.create(
+#             username='Test',
+#             password='Password',
+#             email='test@email.com'
+#         )
+
+#         reservation = Booking.objects.create(
+#             user=test_user,
+#             lead='Test Lead',
+#             email='test@email.com',
+#             mobile='01509',
+#             date='2022-01-25',
+#             time='4:30 P.M.',
+#             notes='test',
+#             guests='2'
+#         )
