ad3cfc950e47c3148fb9f92170aa10719ed0e1e0
LewisCM14 <Lewiscmartin@live.co.uk>
2022-01-28 17:44:09 +0000

    begin defining BookingForm fields

--- booking/forms.py
+++ booking/forms.py
@@ -6,6 +6,45 @@ class BookingForm(forms.ModelForm):
     """
     Creates the BookingForm class that inherits from the base form.
     """
+
+    lead = forms.CharField(
+        label='Name of Booking Lead',
+        required=True,
+    )
+
+    email = forms.EmailField(
+        label='Email Address',
+        required=True,
+    )
+
+    mobile = forms.IntegerField(
+        label='Best Contact Number (Mobile)',
+        required=True,
+    )
+
+    date = forms.DateField(
+        label='Date of Booking',
+        required=True,
+    )
+
+    time = forms.TimeField(
+        label='Arrival Time',
+        required=True,
+    )
+
+    notes = forms.CharField(
+        label='Special Requirements',
+        required=False,
+        widget=forms.Textarea,
+        max_length=300
+    )
+
+    guests = forms.IntegerField(
+        label='Number of Guests',
+        required=True,
+        min_value=1
+    )
+
     class Meta:
         """
         The booking form inherits from the Booking model.
@@ -13,5 +52,5 @@ class BookingForm(forms.ModelForm):
         """
         model = Booking
         fields = (
-            'lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests',
+            'lead', 'email', 'mobile', 'date', 'time', 'notes', 'guests'
         )
