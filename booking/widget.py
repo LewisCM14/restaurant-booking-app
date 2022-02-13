e47da5e44b64f5c6096506833944132d7c2d8749
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-13 12:41:44 +0000

    add notes to widget.py

--- booking/widget.py
+++ booking/widget.py
@@ -3,13 +3,15 @@ from django import forms
 
 class DatePickerInput(forms.DateInput):
     """
-
+    Defines the date picker widget used in the BookingForm.
+    The BookingForm is found in forms.py
     """
     input_type = 'date'
 
 
 class TimePickerInput(forms.TimeInput):
     """
-
+    Defines the time picker widget used in the BookingForm.
+    The BookingForm is found in forms.py
     """
     input_type = 'time'
