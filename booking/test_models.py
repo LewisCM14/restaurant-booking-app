0b127d229af34da7a8969ea4b9f06f58a0edf365
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-21 14:55:36 +0000

    rewrite test_status_detaults_to_pending

--- booking/test_models.py
+++ booking/test_models.py
@@ -7,33 +7,41 @@ from .models import Booking
 
 class TestModel(TestCase):
     """
-    Contains the tests for the model.
+    Contains the tests for the Booking model.
     Located in the booking app in model.py.
     """
 
     def test_status_defaults_to_pending(self):
         """
-        Creates a reservation instance.
-        Then uses assert status is equal to 0,
-        which is pending, as defined in the model file.
+        Creates a User instance for use in testing.
+        Then creates a Booking instance for this test_user.
 
-        This method also tests the str method of the model
-        returns the corret value.
+        This instance is stored in the reservation variable.
+        The status is not set.Then uses assert status is equal to 0
+        which is pending, as defined in the model.py file.
+        Meaning the status field of the Booking model defaults to pending.
+
+        This method also tests that the str method of the Booking model
+        returns the corret value, which is the date and time of the Booking.
+        This is tested with assert equal that str(reservation) is the
+        value for the date and time fields of the reservation instance.
         """
         test_user = User.objects.create(
-            username='Test',
+            username='John',
+            first_name='John',
+            last_name='Doe',
             password='Password',
-            email='test@email.com'
+            email='johndoe@email.com'
         )
 
         reservation = Booking.objects.create(
             user=test_user,
-            lead='Test Lead',
-            email='test@email.com',
-            mobile='01509',
+            lead='John Doe',
+            email='johndoe@email.com',
+            mobile='1509507006',
             date='2022-01-25',
             time='4:30 P.M.',
-            notes='test',
+            notes='none',
             guests='2'
         )
 
--- booking/test_views.py
+++ booking/test_views.py
@@ -1,126 +1,126 @@
-""" This module contains the tests for views.py in the booking directory. """
+# """ This module contains the tests for views.py in the booking directory. """
 
-from django.test import TestCase
-from django.contrib.auth.models import User
-from .models import Booking
+# from django.test import TestCase
+# from django.contrib.auth.models import User
+# from .models import Booking
 
 
-class TestViews(TestCase):
-    """
-    Contains the tests for the views.
-    Located in the booking app in views.py.
-    """
+# class TestViews(TestCase):
+#     """
+#     Contains the tests for the views.
+#     Located in the booking app in views.py.
+#     """
 
-    def setUp(self):
-        """
-        Creates a reservation instance using the Booking model,
-        for use within the test case.
-        """
-        test_user = User.objects.create(
-            username='Test',
-            password='Password',
-            email='test@email.com'
-        )
+#     def setUp(self):
+#         """
+#         Creates a reservation instance using the Booking model,
+#         for use within the test case.
+#         """
+#         test_user = User.objects.create(
+#             username='Test',
+#             password='Password',
+#             email='test@email.com'
+#         )
 
-        Booking.objects.create(
-            user=test_user,
-            lead='Test Lead',
-            email='test@email.com',
-            mobile='01509',
-            date='2022-01-25',
-            time='4:30 P.M.',
-            notes='test',
-            guests='2'
-        )
+#         Booking.objects.create(
+#             user=test_user,
+#             lead='Test Lead',
+#             email='test@email.com',
+#             mobile='01509',
+#             date='2022-01-25',
+#             time='4:30 P.M.',
+#             notes='test',
+#             guests='2'
+#         )
 
-    def test_get_index_page(self):
-        """
-        Uses Django's in-built HTTP client to get the index page URL slash.
-        Asserts equal to status code 200, a successful HTTP response.
+#     def test_get_index_page(self):
+#         """
+#         Uses Django's in-built HTTP client to get the index page URL slash.
+#         Asserts equal to status code 200, a successful HTTP response.
 
-        Then uses assert Template Used to ensure the index.html page,
-        plus the base.html it is extended from, is being used.
-        """
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html', 'base.html')
+#         Then uses assert Template Used to ensure the index.html page,
+#         plus the base.html it is extended from, is being used.
+#         """
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'index.html', 'base.html')
 
-    def test_get_booking_page(self):
-        """
-        Uses Django's in-built HTTP client to get /makebooking URL.
-        Asserts equal to status code 200, a successful HTTP response.
+#     def test_get_booking_page(self):
+#         """
+#         Uses Django's in-built HTTP client to get /makebooking URL.
+#         Asserts equal to status code 200, a successful HTTP response.
 
-        Then uses assert Template Used to ensure the booking.html page,
-        plus the base.html it is extended from, is being used.
-        """
-        response = self.client.get('/makebooking')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'booking.html', 'base.html')
+#         Then uses assert Template Used to ensure the booking.html page,
+#         plus the base.html it is extended from, is being used.
+#         """
+#         response = self.client.get('/makebooking')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'booking.html', 'base.html')
 
-    def test_get_amend_booking_page(self):
-        """
-        Uses Django's in-built HTTP client,
-        to get /amend/1 in the URL. Asserts equal to status code 200,
-        a successful HTTP response.
+#     def test_get_amend_booking_page(self):
+#         """
+#         Uses Django's in-built HTTP client,
+#         to get /amend/1 in the URL. Asserts equal to status code 200,
+#         a successful HTTP response.
 
-        There is only 1 reservation in the database, which is why the URL is,
-        /amend/1
+#         There is only 1 reservation in the database, which is why the URL is,
+#         /amend/1
 
-        Then uses assert Template Used to ensure the amend_booking.html page,
-        plus the base.html it is extended from, is being used.
-        """
-        response = self.client.get('/amend/1')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'amend_booking.html', 'base.html')
+#         Then uses assert Template Used to ensure the amend_booking.html page,
+#         plus the base.html it is extended from, is being used.
+#         """
+#         response = self.client.get('/amend/1')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'amend_booking.html', 'base.html')
 
-    def test_get_reservations_page(self):
-        """
-        Creates a new user and logs in,
-        allowing access to the reservations page.
+#     def test_get_reservations_page(self):
+#         """
+#         Creates a new user and logs in,
+#         allowing access to the reservations page.
 
-        Uses Django's in-built HTTP client to get /reservations URL.
-        Asserts equal to status code 200, a successful HTTP response.
+#         Uses Django's in-built HTTP client to get /reservations URL.
+#         Asserts equal to status code 200, a successful HTTP response.
 
-        Then uses assert Template Used to ensure the reservations.html page,
-        plus the base.html it is extended from, is being used.
-        """
-        User.objects.create_user(
-            username='Test2',
-            password='Password',
-            email='test2@email.com'
-        )
+#         Then uses assert Template Used to ensure the reservations.html page,
+#         plus the base.html it is extended from, is being used.
+#         """
+#         User.objects.create_user(
+#             username='Test2',
+#             password='Password',
+#             email='test2@email.com'
+#         )
 
-        self.client.login(
-            username='Test2',
-            password='Password',
-            email='test2@email.com'
-        )
+#         self.client.login(
+#             username='Test2',
+#             password='Password',
+#             email='test2@email.com'
+#         )
 
-        response = self.client.get('/reservations')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'reservations.html', 'base.html')
+#         response = self.client.get('/reservations')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'reservations.html', 'base.html')
 
-    def test_create_booking_request(self):
-        """
-        Uses the count method to return
-        the total number of reservations in the Booking database.
-        Then assets equal to 1,
-        as there should only be the reservation made in the setUp method.
-        """
-        count = Booking.objects.count()
-        self.assertEqual(count, 1)
+#     def test_create_booking_request(self):
+#         """
+#         Uses the count method to return
+#         the total number of reservations in the Booking database.
+#         Then assets equal to 1,
+#         as there should only be the reservation made in the setUp method.
+#         """
+#         count = Booking.objects.count()
+#         self.assertEqual(count, 1)
 
-    def test_cancel_booking_request(self):
-        """
-        Removes the reservation created in the setUp method
-        using the cancel URL with id 1.
+#     def test_cancel_booking_request(self):
+#         """
+#         Removes the reservation created in the setUp method
+#         using the cancel URL with id 1.
 
-        Then filters the Booking database by this id.
-        asserts the length of the variable this is stored in
-        is equal to 0, meaning the reservation has been removed
-        from the database.
-        """
+#         Then filters the Booking database by this id.
+#         asserts the length of the variable this is stored in
+#         is equal to 0, meaning the reservation has been removed
+#         from the database.
+#         """
 
-        self.client.get('/cancel/1')
-        existing_reservation = Booking.objects.filter(id=1)
-        self.assertEqual(len(existing_reservation), 0)
+#         self.client.get('/cancel/1')
+#         existing_reservation = Booking.objects.filter(id=1)
+#         self.assertEqual(len(existing_reservation), 0)
