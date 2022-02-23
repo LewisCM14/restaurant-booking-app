959a3aee6783923939d390a7d5e743352e2dff7d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-23 16:20:06 +0000

    create MenuStarter model, register in admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+""" This module contains the admin logic for the menu app. """
 
-# Register your models here.
+from django.contrib import admin
+from .models import MenuStarter
+
+
+@admin.register(MenuStarter)
+class MenuStarterAdmin(admin.ModelAdmin):
+    """
+    The Admin panel for the starters on the menu model.
+    """
+    list_display = ('title',)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+""" This module contains the database model for the menu app. """
 
-# Create your models here.
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+
+class MenuStarter(models.Model):
+    """
+    The model for the starters on the menu app.
+
+    Stores the title of each item plus a brief description
+    and the price. An image for the item is stored as a
+    cloudinary field.
+
+    The title must be unique to prevent the same item being
+    added multiple times. The price field uses a decimal field
+    to ensure a valid price format can be added.
+    """
+    title = models.CharField(max_length=100, blank=False, unique=True)
+    description = models.TextField(max_length=200, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
+    image = CloudinaryField('image')
+
+    class Meta:
+        """
+        Orders the starter items alphabetically by title.
+        """
+        ordering = ['title']
+
+    def __str__(self):
+        """
+        Returns the starter items title as a string.
+        """
+        return f'{self.title}'
