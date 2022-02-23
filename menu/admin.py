c02cd73dc0e64368f42a2e6137d4a51acd956a7a
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-23 17:53:04 +0000

    create Dessert model in menu app, register in admin

--- menu/admin.py
+++ menu/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Starter, Main
+from .models import Starter, Main, Dessert
 
 
 @admin.register(Starter)
@@ -13,10 +13,20 @@ class StarterAdmin(SummernoteModelAdmin):
     summernote_fields = ('description')
     list_display = ('title',)
 
+
 @admin.register(Main)
 class MainAdmin(SummernoteModelAdmin):
     """
     The Admin panel for the mains on the menu model.
+    """
+    summernote_fields = ('description')
+    list_display = ('title',)
+
+
+@admin.register(Dessert)
+class DessertAdmin(SummernoteModelAdmin):
+    """
+    The Admin panel for the desserts on the menu model.
     """
     summernote_fields = ('description')
     list_display = ('title',)
