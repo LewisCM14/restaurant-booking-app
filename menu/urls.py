b41cec39d425f60fa32ccfe126b427f0b826ff2d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-23 19:44:48 +0000

    create urls for menu tabs, link in navbar

--- menu/urls.py
+++ menu/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('menu', views.menu, name='menu'),
+    path('menu', views.Starters.as_view(), name='starters'),
+    path('menu', views.Mains.as_view(), name='mains'),
+    path('menu', views.Desserts.as_view(), name='desserts'),
+    path('menu', views.Drinks.as_view(), name='drinks'),
 ]
