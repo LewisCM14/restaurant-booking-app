dc2851e347026bdfb036a53703e195ad4f08046c
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-24 17:53:00 +0000

    expand notes on views.py in the menu app

--- menu/views.py
+++ menu/views.py
@@ -9,7 +9,10 @@ class StarterList(generic.ListView):
     Class based view using generic for the Starter model
     found in models.py on the menu app.
 
-    Renders on the starters.html template.
+    Queries entries within the Starter database by those
+    that have the display field set to 'on'.
+
+    Renders the results on the starters.html template.
     """
     model = Starter
     queryset = Starter.objects.filter(display=1).order_by('title')
@@ -21,7 +24,10 @@ class MainList(generic.ListView):
     Class based view using generic for the Main model
     found in models.py on the menu app.
 
-    Renders on the mains.html template.
+    Queries entries within the Main database by those
+    that have the display field set to 'on'.
+
+    Renders the results on the mains.html template.
     """
     model = Main
     queryset = Main.objects.filter(display=1).order_by('title')
@@ -33,7 +39,10 @@ class DessertList(generic.ListView):
     Class based view using generic for the Dessert model
     found in models.py on the menu app.
 
-    Renders on the dessert.html template.
+    Queries entries within the Dessert database by those
+    that have the display field set to 'on'.
+
+    Renders the results on the dessert.html template.
     """
     model = Dessert
     queryset = Dessert.objects.filter(display=1).order_by('title')
