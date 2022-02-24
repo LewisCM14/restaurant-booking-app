2a7e836cf679c1dcac1d4e18b256d80b1c8d1d7d
LewisCM14 <Lewiscmartin@live.co.uk>
2022-02-24 17:44:47 +0000

    add notes to apps.py in menu app

--- menu/apps.py
+++ menu/apps.py
@@ -1,6 +1,11 @@
+""" This module configures the menu app. """
+
 from django.apps import AppConfig
 
 
 class MenuConfig(AppConfig):
+    """
+    Sets the variables needed for the menu app configuration.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'menu'
